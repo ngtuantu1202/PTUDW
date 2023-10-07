f0573aa77b9939a82e17f1f6ce1cae128fb9a6c7
ngtuantu1202 <125531012+ngtuantu1202@users.noreply.github.com>
2023-10-07 19:23:02 +0700

    Lam tiep UDW_Category_Index_FunctionDAO
    v3

---
+++ BanHang/BanHang/Controllers/SiteController.cs
@@ -0,0 +1,21 @@
+﻿using MyClass.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BanHang.Controllers
+{
+    public class SiteController : Controller
+    {
+        // GET: Site
+        public ActionResult Index()
+        {
+            MyDBContext db = new MyDBContext();
+            int count = db.Products.Count();
+            ViewBag.choi = count;
+            return View();
+        }
+    }
+}
