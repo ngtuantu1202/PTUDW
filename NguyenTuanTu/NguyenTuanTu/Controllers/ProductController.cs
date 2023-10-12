eef100c94efbebf0003c7e249afd65a93f380390
ngtuantu1202 <125531012+ngtuantu1202@users.noreply.github.com>
2023-10-12 11:25:39 +0700

    Kiem Tra
    BanHang

---
+++ NguyenTuanTu/NguyenTuanTu/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using NguyenTuanTu.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NguyenTuanTu.Controllers
+{
+    public class ProductController : Controller
+    {
+        private List<Product> GetSampleProductList()
+        {
+            return new List<Product>
+    {
+        new Product { Id = 1, Name = "Bàn phím ReZero", ImageUrl = "sp1.jpg", Price = 500 },
+        new Product { Id = 2, Name = "Bàn phím đen trắng", ImageUrl = "sp2.jpg", Price = 450 },
+        new Product { Id = 3, Name = "Bàn phím Hutao", ImageUrl = "sp3.jpg", Price = 1000 }
+    };
+        }
+
+
+        public ActionResult Index()
+        {
+            List<Product> productList = GetSampleProductList();
+            return View(productList);
+        }
+
+        public ActionResult Details(int id)
+        {
+            Product product = GetSampleProductList().FirstOrDefault(p => p.Id == id);
+            return View(product);
+        }
+    }
+
+}
